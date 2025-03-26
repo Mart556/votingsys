032bf6c4f2862dd469f555b7f2a14aedf8566076
Mart Haamer <49863634+Mart556@users.noreply.github.com>
2025-03-26 13:31:25 +0200

    Enhance vote logging by adding user and session tracking in database triggers and update LOGI table schema

--- db.sql
+++ db.sql
@@ -19,36 +19,28 @@ CREATE TRIGGER after_haaletus_update
 AFTER UPDATE ON haaletus
 FOR EACH ROW 
 BEGIN
-  -- Log the vote change
-  IF NEW.vote IS NOT NULL AND OLD.vote IS NULL THEN
-    -- Vote created for the first time
-    INSERT INTO LOGI (vote_change, status)
-    VALUES (NEW.vote, 'created');
-    
-    -- Update the voting results
-    IF (SELECT COUNT(*) FROM TULEMUSED) = 0 THEN
-      -- Create new voting session if none exists
-      INSERT INTO TULEMUSED (for_count, against_count)
-      VALUES (IF(NEW.vote = 1, 1, 0), IF(NEW.vote = 0, 1, 0));
-    ELSE
-      -- Update existing voting session
-      UPDATE TULEMUSED 
-      SET for_count = for_count + IF(NEW.vote = 1, 1, 0),
-        against_count = against_count + IF(NEW.vote = 0, 1, 0)
-      ORDER BY voting_start DESC LIMIT 1;
-    END IF;
-    
-  ELSEIF NEW.vote IS NOT NULL AND OLD.vote IS NOT NULL AND OLD.vote != NEW.vote THEN
-    -- Vote changed
-    INSERT INTO LOGI (vote_change, status)
-    VALUES (NEW.vote, 'updated');
-    
-    -- Update the voting results
-    UPDATE TULEMUSED
-    SET for_count = for_count + IF(NEW.vote = 1, 1, -1),
-      against_count = against_count + IF(NEW.vote = 0, 1, -1)
-    ORDER BY voting_start DESC LIMIT 1;
-  END IF;
+	SET @latest_tulemused_id = (SELECT id FROM TULEMUSED ORDER BY voting_start DESC LIMIT 1);
+
+	IF NEW.vote IS NOT NULL AND OLD.vote IS NULL THEN
+		INSERT INTO LOGI (vote_change, status, user_id, session_id)
+		VALUES (NEW.vote, 'created', NEW.id, @latest_tulemused_id);
+
+		UPDATE TULEMUSED
+			SET for_count = for_count + IF(NEW.vote = 1, 1, 0),
+			against_count = against_count + IF(NEW.vote = 0, 1, 0)
+			ORDER BY voting_start DESC LIMIT 1;
+
+	ELSEIF NEW.vote IS NOT NULL AND OLD.vote IS NOT NULL AND OLD.vote != NEW.vote THEN
+		-- Vote changed
+		INSERT INTO LOGI (vote_change, status, user_id, session_id)
+		VALUES (NEW.vote, 'updated', NEW.id, @latest_tulemused_id);
+		
+		-- Update the voting results
+		UPDATE TULEMUSED
+		SET for_count = for_count + IF(NEW.vote = 1, 1, -1),
+		against_count = against_count + IF(NEW.vote = 0, 1, -1)
+		ORDER BY voting_start DESC LIMIT 1;
+	END IF;
 END//
 DELIMITER ;
 
@@ -75,11 +67,16 @@ CREATE TABLE TULEMUSED (
   INDEX idx_voting_time (voting_start)
 );
 
+
 -- Create the LOGI (logs) table
 CREATE TABLE LOGI (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  user_id INT UNSIGNED,
+  session_id INT UNSIGNED,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   vote_change TINYINT(1) NOT NULL,
   status ENUM('updated', 'created') NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES HAALETUS(id),
+  FOREIGN KEY (session_id) REFERENCES TULEMUSED(id),
   INDEX idx_updated_at (updated_at)
 );
